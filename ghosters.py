64768455d3008fcc1b71b8deca46704bc113b3eb
Abhishek <abhins4@gmail.com>
2020-06-29 16:36:35 +0530

    Create ghosters.py

---
+++ ghosters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def myHumbleRequest():
+    '''
+    Hello Corporate Ghosters!!!
+    '''
+
+    if_you_are_a = [
+        'Recruiter',
+        'Principal Recruiter',
+        'Talent Advisor',
+        'Engg. Director',
+        'Senior Engg. Manager',
+        'Blah-Blah-Blah...',
+    ]
+
+    if 'you_broadcast_on_LinkedIn' \
+            or 'you_approach_potential_candidates':
+        for jobs in 'your_respective_companies':
+            if 'candidate_responded_you_on_LinkedIn' \
+                    or 'the_interview_feedback_is_pending':
+                at_least_reply_in_a = ['YES', 'NO']
+
+    # you should execute this program to see what it says about...
+    your_corporate_ethics
